040571b6e9f85ea78bd21b9a6852dd3a6df9c9c5
8bitsats <8bit@ordlibrary.com>
2024-06-01 00:35:54 -0400

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+export default defineNuxtConfig({
+  modules: ["@nuxtjs/tailwindcss"],
+  vite: {
+    esbuild: {
+      target: "esnext",
+    },
+    build: {
+      target: "esnext",
+    },
+    optimizeDeps: {
+      include: ["@project-serum/anchor", "@solana/web3.js", "buffer"],
+      esbuildOptions: {
+        target: "esnext",
+      },
+    },
+    define: {
+      "process.env.BROWSER": true,
+    },
+  },
+});
